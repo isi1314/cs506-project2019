62ce70a7cec61a767f500b37265e78afaddcf630
isi1314 <isidora@bu.edu>
2019-10-28 14:20:26 -0400

    adding data & scripts

---
+++ scripts/read_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from pandas import *
+
+import urllib
+from urllib import pathname2url
+from urlparse import urlparse
+
+#reading dataset
+df = pd.read_csv('../datasets/data_all_no_naics.csv', index_col=0)
+
+
+#changin column names
+df.rename(columns={'unknown':'Contact'}, inplace=True)
+df.rename(columns={'unknown.1':'Position'}, inplace=True)
+df.rename(columns={'Address, line 1':'Address_line_1'}, inplace=True)
+df.rename(columns={'Address, line 2':'Address_line_2'}, inplace=True)
+df.rename(columns={'Capabilities Narrative':'Capabilities_narrative'}, inplace=True)
+df.rename(columns={'Name of Firm':'Name_of_firm'}, inplace=True)
+df.rename(columns={'Trade Name':'Trade_name'}, inplace=True)
+
+#dropping nan values
+df.dropna(subset=['Contact'],axis='rows',inplace=True)
+
+
+#percent encoding Contact names to be used in naming algorithm
+names_array = df['Contact'].values
+urls_list=[]
+for name in names_array:
+	urls_list.append(urllib.pathname2url(name))
+
+
+df['perc_encoding']=urls_list
+
+#save to csv
+df.to_csv('../datasets/data_all_no_naics_prc.csv')
