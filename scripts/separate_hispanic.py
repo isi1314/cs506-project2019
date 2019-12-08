81dcb1e43ad20f938cdd9033fbe7f0c43bb587bb
Isidora Tourni <isidora@bu.edu>
2019-12-08 15:54:53 -0500

    Update separate_hispanic.py

--- scripts/separate_hispanic.py
+++ scripts/separate_hispanic.py
@@ -1,3 +1,5 @@
+#keeping only hispanic entries from SBA cleaned dataset
+
 import numpy as np
 import pandas as pd
 
@@ -7,7 +9,7 @@ from pandas import *
 #reading dataset
 df = pd.read_csv('../datasets/small_disadvantaged_proc_owners_full_no_duplicates_fin_names.csv', index_col=0)
 
-
+#filtering hispanic entries
 df_new = df[df['race_ethnicity_alt']=='HL']
 
 #save to csv
